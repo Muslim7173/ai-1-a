9a5bd9726f449028fdc573621126dd299483bd7e
Muslimbek <muslm19990105@gmail.com>
2025-06-09 18:02:02 +0900

    requirementlar_qoshdim

--- src/logger.py
+++ src/logger.py
@@ -3,16 +3,17 @@ import os
 from datetime import datetime
 
 
-LOG_FILE=f"{datetime.now().strftime('%m_%d,%_Y,%_H,%_M,_%_S')}.log"
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+
 
 log_path=os.path.join(os.getcwd(), "log", LOG_FILE)
 
 os.makedirs(log_path,exist_ok=True)
 
-LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
+LOG_FILE_PATH=os.path.join(log_path,LOG_FILE)
 
 logging.basicConfig(
-    filename=LOG_FILE_PATH
+    filename=LOG_FILE_PATH,
     format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s -%(message)s ",
     level=logging.INFO
     )
